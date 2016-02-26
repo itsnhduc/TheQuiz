5ebe688807f441750920917b9c18f446946e2e78
Mike Nguyen <nhduc.lhp@gmail.com>
2016-02-26 19:20:23 +0800

    Change in Facebook app detail

--- server/social-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
- 
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '746753392091944',
-    secret: '38a2db37ab4d5fcb7fe72b4c9e1ae7e5'
-});
